77e515ba1d4e4e827370c51b85e6cf5faa9976bf
Fred Medlin <fred@medl.in>
2015-03-17 01:33:38 -0400

    Add test environment and a failing test

---
+++ app/src/test/java/com/fredmedlin/wikilooks/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.fredmedlin.wikilooks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.fail;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = "app/src/main/AndroidManifest.xml", emulateSdk = 18)
+public class MainActivityTest {
+
+    MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void itShouldFail() {
+        fail("Implement me");
+    }
+}
