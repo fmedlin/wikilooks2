da442d7ce7798189cac1272be776e1d3035fbbd3
Fred Medlin <fred@medl.in>
2015-03-21 21:25:41 -0400

    Scaffold model tests

---
+++ app/src/test/java/com/fredmedlin/wikilooks/ui/mvp/model/ArticlesModelTest.java
@@ -0,0 +1,22 @@
+package com.fredmedlin.wikilooks.ui.mvp.model;
+
+import junit.framework.TestCase;
+
+public class ArticlesModelTest extends TestCase {
+
+    public void testRequestLocation() throws Exception {
+
+    }
+
+    public void testFetchArticlesByLocation() throws Exception {
+
+    }
+
+    public void testFetchArticle() throws Exception {
+
+    }
+
+    public void testGetArticle() throws Exception {
+
+    }
+}
